47fd5ae830b827f7b738dfd5ddd4f7288bd26628
RodrigoVitor <rodrigovitordev2024@gmail.com>
2026-02-02 13:29:30 -0300

    concertando link para criar conta na tela de login

--- resources/views/livewire/pages/auth/login-candidate.blade.php
+++ resources/views/livewire/pages/auth/login-candidate.blade.php
@@ -59,9 +59,14 @@ new #[Layout('layouts.guest')] class extends Component {
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                {{-- <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                     href="{{ route('password.request') }}" wire:navigate>
                     {{ __('Esqueceu sua senha?') }}
+                </a> --}}
+
+                <a class="text-white hover:text-gray-400"
+                    href="{{ route('register-candidate') }}" wire:navigate>
+                    {{ __('Criar uma contra') }}
                 </a>
             @endif
 
